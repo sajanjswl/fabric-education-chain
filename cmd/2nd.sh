93fbb22ded92b02199c6ba691b2f928f218f8610
Sajan Jaiswal <sjnjaiswal@gmail.com>
2021-06-25 14:34:49 +0530

    Adding Dapp

--- cmd/2nd.sh
+++ cmd/2nd.sh
@@ -3,7 +3,8 @@
 
 
 
-docker exec -e "CORE_PEER_MSPCONFIGPATH=/tmp/hyperledger/org1/admin/msp" -e "CORE_PEER_ADDRESS=peer1-org1:7051" cli-org1 peer lifecycle chaincode approveformyorg -o orderer1-org0:7050  --channelID mychannel --name basic --version 1.0 --package-id basic_1.0:42f7fb34a6f8f08d6591ac0a494facd5b779072c80670ee4e70c45dcce1bd147 --sequence 1 --tls --cafile /tmp/hyperledger/org1/peer1/tls-msp/tlscacerts/tls-0-0-0-0-7052.pem
+docker exec -e "CORE_PEER_MSPCONFIGPATH=/tmp/hyperledger/org1/admin/msp" -e "CORE_PEER_ADDRESS=peer1-org1:7051" \
+cli-org1 peer lifecycle chaincode approveformyorg -o orderer1-org0:7050  --channelID mychannel --name basic --version 1.0 --package-id basic_1.0:42f7fb34a6f8f08d6591ac0a494facd5b779072c80670ee4e70c45dcce1bd147 --sequence 1 --tls --cafile /tmp/hyperledger/org1/peer1/tls-msp/tlscacerts/tls-0-0-0-0-7052.pem
 sleep 2s
 echo "                                                                "
 echo "################################################################"
---
+++ dapp/go.mod
@@ -0,0 +1,3 @@
+module github.com/sajanjswl/dapp
+
+go 1.16
